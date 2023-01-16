a7f598a5c37712f2c7c704d2f56fe0c163106cea
BohdanBru <mattewgreenman@gmail.com>
2023-01-16 12:34:33 +0100

    Add assembly plugin, start scripts. Update NOTICE

---
+++ src/main/scripts/start.cmd
@@ -0,0 +1,8 @@
+@rem The cmd script to start tic-tac-toe game for Windows system
+@rem author Bohdan Brukhovets
+@rem emaul: mattewgreenman@gmail.com
+@rem linkdin: https://www.linkedin.com/in/bohdan-brukhovets/
+@ echo off
+java -jar ${project.build.finalName}.jar
+
+@rem C:\Users\User\IdeaProjects\tic-tac-toe-maven\out\tic-tac-toe.jar
---
+++ src/main/scripts/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# The cmd script to start tic-tac-toe game for Unix system
+# author Bohdan Brukhovets
+# emaul: mattewgreenman@gmail.com
+# linkdin: https://www.linkedin.com/in/bohdan-brukhovets/
+java -jar ${project.build.finalName}.jar
