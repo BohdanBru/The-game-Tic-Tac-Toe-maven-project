7000af5f39491550d64cacb5a6e59b4bed39617b
BohdanBru <mattewgreenman@gmail.com>
2023-01-20 16:09:10 +0100

    Update start scripts, description files. Add proguard-ruls.

--- src/main/scripts/start.cmd
+++ src/main/scripts/start.cmd
@@ -3,7 +3,39 @@
 @rem emaul: mattewgreenman@gmail.com
 @rem linkdin: https://www.linkedin.com/in/bohdan-brukhovets/
 @ echo off
-java -jar ${project.build.finalName}.jar
+setlocal
+@rem---------------------------------------------------------------------------------------
+@rem Try to use the java.exe from %JAVA_HOME%, if this environment variable set correctly:
+if %JAVA_HOME% neq ""
+    if exist %JAVA_HOME%\bin\java.exe (
+        set JAVA_CMD=%JAVA_HOME%\bin\java
+        )
+@rem---------------------------------------------------------------------------------------
+@rem Tryto use the java.exe using 'PATH' environment variable:
+where /Q java
+if %ERRORLEVEL% == 0 (
+    set JAVA_CMD=java
+)
+@rem----------------------------------------------------------------------------------------
+@rem Try to use java.exe from JRE if jre\bin\java.exe exist:
+if exist jre\bin\java.exe(
+    set JAVA_CMD=jre\bin\java
+    )
+@rem----------------------------------------------------------------------------------------
+if not defined JAVA_CMD (
+@rem Throw error if java.exe is not configured:
+    echo------------------------------------------------------------------------------------
+    echo java.exe not defined! install or configure JVM before using this script!
+    set RETURN_CODE=1
+) else (
+@rem Run tic-tac-toe game:
+%JAVA_CMD% -jer ${project.build.finalName}-release.jar
+set RETURN_CODE=0
+java -jar ${project.build.finalName}-release.jar
+)
+@rem----------------------------------------------------------------------------------------
+@rem Wait for the any key pressed:
 pause
-
+@rem----------------------------------------------------------------------------------------
+exit /b %RETURN_CODE%
 @rem C:\Users\User\IdeaProjects\tic-tac-toe-maven\out\tic-tac-toe.jar
--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -3,8 +3,41 @@
 # author Bohdan Brukhovets
 # emaul: mattewgreenman@gmail.com
 # linkdin: https://www.linkedin.com/in/bohdan-brukhovets/
+###################################################################################
+#----------------------------------------------------------------------------------
+# Try to use the java from '$JAVA_HOME' if this environment variable set cerrectly:
+if [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ] ; then
+  JAVA_CMD="$JAVA_HOME/bin/java"
+  fi
+#-----------------------------------------------------------------------------------
+#Try to use the java using PATH environment variable:
+WHITH_JAVA=$(while java)
+if [ -x "$WHICH_JAVA" ]; then
+  JAVA_CMD="java"
+  unset WHICH_JAVA
+  fi
+#------------------------------------------------------------------------------------
+if [ -z ${JAVA_CMD+x} ]; then
+  #Throw error if java is not configured:
+  echo "------------------------------------------------------------------------------"
+  echo "\'java\' not defined! Install or configure JVM before using script!"
+  echo "------------------------------------------------------------------------------"
+  RETURN_CODE=1
+  else
+    # Fix current dir issue
+    cd "$(dirname "$0")" || exit
+    #Run tic-tac-toe game:
+    $JAVA_CMD -jar ${project.build.finalName}-release.jar
+    RETURN_CODE=0
+    fi
+    #----------------------------------------------------------------------------------
+    # Wait for the enter key pressed:
+    echo "Press enter to continue . . ."
+    read -r test
+    #----------------------------------------------------------------------------------
+    exit $RETURN_CODE
 
-cd "$(dirname "$0")" || exit
-java -jar ${project.build.finalName}.jar
-echo "Please, press ENTER to continue . . . "
-read -r test
+# cd "$(dirname "$0")" || exit
+# java -jar ${project.build.finalName}-release.jar
+# echo "Please, press ENTER to continue . . . "
+# read -r test
